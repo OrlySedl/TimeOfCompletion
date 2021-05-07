80ce729dac7f8caf21e145c95999cba611efaeb3
OrlySedl <orlysedl@gmail.com>
2021-05-07 22:18:14 +0300

    Добавлена функция StackStockSpan.

--- TimeOfCompletion/Program.cs
+++ TimeOfCompletion/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace TimeOfCompletion
 {
@@ -28,6 +29,31 @@ namespace TimeOfCompletion
                     }
                 }
                 spans[i] = k;
+            }
+            return spans;
+        }
+
+        static int[] StackStockSpan(int[] quotes)
+        {
+            int[] spans = new int[quotes.Length];
+            spans[0] = 1;
+            Stack<int> stack = new Stack<int>();
+            stack.Push(0);
+            for (int i = 1; i < quotes.Length; i++)
+            {
+                while (stack.Count != 0 && quotes[stack.Peek()] <= quotes[i])
+                {
+                    stack.Pop();
+                }
+                if (stack.Count == 0)
+                {
+                    spans[i] = i + 1;
+                }
+                else
+                {
+                    spans[i] = i - stack.Peek()
+                }
+                stack.Push(i);
             }
             return spans;
         }
