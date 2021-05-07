adf26cbcaeacb2e087d6ee3bcb2dd08c528885b9
OrlySedl <orlysedl@gmail.com>
2021-05-07 20:54:18 +0300

    Добавлены файлы проекта.

---
+++ TimeOfCompletion/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TimeOfCompletion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
