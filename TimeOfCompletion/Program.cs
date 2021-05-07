56c365d4e3130b259b7231df9de44dffe65ff453
OrlySedl <orlysedl@gmail.com>
2021-05-07 21:13:37 +0300

    Добавлена функция SimpleStockSpan.

--- TimeOfCompletion/Program.cs
+++ TimeOfCompletion/Program.cs
@@ -8,5 +8,28 @@ namespace TimeOfCompletion
         {
             Console.WriteLine("Hello World!");
         }
+
+        static int[] SimpleStockSpan(int[] quotes)
+        {
+            int[] spans = new int[quotes.Length];
+            for (int i = 0; i < quotes.Length; i++)
+            {
+                int k = 1;
+                bool spanEnd = false;
+                while (i - k >= 0 && !spanEnd)
+                {
+                    if (quotes[i - k] <= quotes[i])
+                    {
+                        k = k + 1;
+                    }
+                    else
+                    {
+                        spanEnd = true;
+                    }
+                }
+                spans[i] = k;
+            }
+            return spans;
+        }
     }
 }
